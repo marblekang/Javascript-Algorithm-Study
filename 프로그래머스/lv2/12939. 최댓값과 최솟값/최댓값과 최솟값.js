19089231479e1baaa7cbe6bc95c6c76a5a84d5d8
Byoungmin Kang <87058243+marblekang@users.noreply.github.com>
2022-10-24 17:21:31 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,12 +1,4 @@
 function solution(s) {
-    let answer = [];
-    let nums = s.split(' ').map((val)=>val*1);
-    let min = Math.min(...nums);
-    let max = Math.max(...nums);
-    answer.push(min);
-    answer.push(max);
-    return answer.join(' ')
-    
-  
-
+    let nums = s.split(' ');
+    return Math.min(...nums)+" "+Math.max(...nums)
 }
