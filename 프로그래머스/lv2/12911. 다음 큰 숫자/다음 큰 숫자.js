96396edeb20751c0e7f92052d11309cb38c9b596
Byoungmin Kang <87058243+marblekang@users.noreply.github.com>
2022-10-28 14:00:27 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.05 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,10 +1,13 @@
 function solution(n) {
     
-     let len = n.toString(2).split("0").join('').length;
+     let len = n.toString(2).split("0").join('');
+    
 while(true){
     n++
-    if(len === n.toString(2).split("0").join('').length){
+    if(len === n.toString(2).split("0").join('')){
         return n
     }
 
-}}
+}
+
+}
