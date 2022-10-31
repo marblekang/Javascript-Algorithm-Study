bb64250c21bebabc28b4c5f8cd7876135ab1cfa7
Byoungmin Kang <87058243+marblekang@users.noreply.github.com>
2022-10-31 15:49:53 +0900

    [level 2] Title: 올바른 괄호, Time: 2.54 ms, Memory: 37 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,10 @@
+function solution(s){
+if(s[s.length-1]!=")") return false;
+    
+let result = 0;
+    for(let i = 0; i < s.length; i++){
+        s[i] === "(" ? result++:result--;
+        if(result < 0) return false;
+    }
+    return (result)?  false : true;
+}
